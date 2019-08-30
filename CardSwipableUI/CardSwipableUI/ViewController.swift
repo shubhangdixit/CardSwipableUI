cbf16e8ffcbc16c403170aefd38f156eae01b9fd
Shubhang <subhangdxt@gmail.com>
2019-08-30 17:42:09 +0530

    Project Setup

---
+++ CardSwipableUI/CardSwipableUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardSwipableUI
+//
+//  Created by Shubhang Dixit on 30/08/19.
+//  Copyright © 2019 Shubhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
