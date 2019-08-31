3ae74f2bfa1a549f93fea5286914ec517ab5ae3b
Shubhang <subhangdxt@gmail.com>
2019-08-31 17:49:14 +0530

    Fixed swipe gesture logic

--- CardSwipableUI/CardSwipableUI/SwipableCollectionViewCell.swift
+++ CardSwipableUI/CardSwipableUI/SwipableCollectionViewCell.swift
@@ -11,10 +11,6 @@ enum ButtonStackState {
     case left, right
 }
 
-enum cardState {
-    case leftSwiped, rightSwiped , notSwiped
-}
-
 class SwipableCollectionViewCell: UICollectionViewCell, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var baseView: UIView!
@@ -24,8 +20,8 @@ class SwipableCollectionViewCell: UICollectionViewCell, UIGestureRecognizerDeleg
     @IBOutlet weak var buttonsStackLeadingConstraint : NSLayoutConstraint!
     @IBOutlet weak var buttonsStackTrailingConstraint: NSLayoutConstraint!
     
-    var buttonsStackState : ButtonStackState = .right
-    
+    var swipableCardOriginX : CGFloat = 0    // Stores initial origin before swipe begins
+    var buttonsStackState : ButtonStackState = .right    // state of stack view , left or Right
     var buttons: [(name: String, type: ButtonType)] = []
     var pan: UIPanGestureRecognizer!
     
@@ -75,36 +71,73 @@ class SwipableCollectionViewCell: UICollectionViewCell, UIGestureRecognizerDeleg
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        
-        
     }
     
     @objc func onPan(_ pan: UIPanGestureRecognizer) {
         let velocity = pan.velocity(in: self)
+        let initialX = baseView.frame.origin.x
+        
+        let swipableLength = mainView.frame.width / 2
+        let width = self.mainView.frame.width
+        let height = self.mainView.frame.height
         switch pan.state {
         case .began:
+            swipableCardOriginX = mainView.frame.origin.x
+            if swipableCardOriginX == baseView.frame.origin.x {
             if velocity.x > 0 {
                 changeButtonStackState(to: .left)
             } else {
                 changeButtonStackState(to: .right)
             }
+            }
         case .changed:
-            let p: CGPoint = pan.translation(in: self)
-            let width = self.mainView.frame.width
-            let height = self.mainView.frame.height
-            if velocity.x < 0 && p.x > baseView.frame.width/2 {
-                self.mainView.frame = CGRect(x: p.x,y: 0, width: width, height: height)
+            let p: CGPoint = pan.translation(in: mainView)
+            if p.x >= (initialX - swipableLength) && p.x <= (initialX + swipableLength) {
+                self.mainView.frame = CGRect(x: swipableCardOriginX + p.x, y: 0, width: width, height: height)
             }
-            if velocity.x > 0 && p.x < baseView.frame.width/2 {
-                self.mainView.frame = CGRect(x: p.x,y: 0, width: width, height: height)
-            }
-            self.setNeedsLayout()
+            
+        self.setNeedsLayout()
         case .ended:
-            break
+            let finalX = mainView.frame.origin.x
+            let p = swipableLength / 5
+            if velocity.x > 0 {
+                if finalX < (initialX - (p * 4)) {
+                    //self.mainView.frame = CGRect(x: initialX - swipableLength, y: 0, width: width, height: height)
+                    animateSwipe(toPosition: initialX - swipableLength)
+                } else if finalX >= (initialX - (p * 4)) && finalX < p {
+                   // self.mainView.frame = CGRect(x: initialX, y: 0, width: width, height: height)
+                    animateSwipe(toPosition: initialX)
+                } else {
+                   // self.mainView.frame = CGRect(x: initialX + swipableLength, y: 0, width: width, height: height)
+                    animateSwipe(toPosition: initialX + swipableLength)
+                }
+            } else {
+                if finalX > (initialX + (p * 4)) {
+                    //self.mainView.frame = CGRect(x: initialX + swipableLength, y: 0, width: width, height: height)
+                    animateSwipe(toPosition: initialX + swipableLength)
+                } else if finalX >= (initialX - p) && finalX <= (initialX + (p * 4))  {
+                   // self.mainView.frame = CGRect(x: initialX, y: 0, width: width, height: height)
+                    animateSwipe(toPosition: initialX)
+                } else {
+                   // self.mainView.frame = CGRect(x: initialX - swipableLength, y: 0, width: width, height: height)
+                    animateSwipe(toPosition: initialX - swipableLength)
+                }
+            }
         case .cancelled:
             break
         default:
             break
+        }
+    }
+    
+    func animateSwipe(toPosition x:CGFloat) {
+        let width = self.mainView.frame.width
+        let height = self.mainView.frame.height
+        UIView.animate(withDuration: 0.3, delay: 0.1, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            self.mainView.frame = CGRect(x: x, y: 0, width: width, height: height)
+            self.contentView.layoutIfNeeded()
+        }) { (_) in
+            // Follow up animations...
         }
     }
     
